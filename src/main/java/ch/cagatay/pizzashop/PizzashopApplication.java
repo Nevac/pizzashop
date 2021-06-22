d89f74fd8ecceed516ecbe18b2bfc8f0500c7c24
Cagatay Suhan Özyurt <cagatayoezyurt@gmail.com>
2021-06-23 01:05:28 +0200

    Initialized Spring Boot Project with resources: - Database schema - OpenAPI file

---
+++ src/main/java/ch/cagatay/pizzashop/PizzashopApplication.java
@@ -0,0 +1,13 @@
+package ch.cagatay.pizzashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzashopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzashopApplication.class, args);
+	}
+
+}
