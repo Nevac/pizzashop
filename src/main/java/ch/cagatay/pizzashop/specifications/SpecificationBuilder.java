832c8d7081bc912d6cb88de1cc05cb800f308dfe
Cagatay Suhan Özyurt <cagatayoezyurt@gmail.com>
2021-06-29 23:25:42 +0200

    Fixed OR query

--- src/main/java/ch/cagatay/pizzashop/specifications/SpecificationBuilder.java
+++ src/main/java/ch/cagatay/pizzashop/specifications/SpecificationBuilder.java
@@ -73,10 +73,12 @@ public class SpecificationBuilder<T> {
         SpecificationBuilder<V> builder = new SpecificationBuilder<>();
         String operationSetExper = Joiner.on("|")
                 .join(SearchOperation.SIMPLE_OPERATION_SET);
-        Pattern pattern = Pattern.compile("(\\w+?)(" + operationSetExper + ")(\\p{Punct}?)(\\w+?)(\\p{Punct}?),");
+        Pattern pattern = Pattern
+                .compile("(\\p{Punct}?)(\\w+?)(" + operationSetExper + ")(\\p{Punct}?)(\\w+?)(\\p{Punct}?),");
         Matcher matcher = pattern.matcher(search + ",");
         while (matcher.find()) {
-            builder.with(matcher.group(1), matcher.group(2), matcher.group(4), matcher.group(3), matcher.group(5));
+            builder.with(matcher.group(1), matcher.group(2), matcher.group(3), matcher.group(5), matcher.group(4),
+                    matcher.group(6));
         }
 
         return builder.build();
